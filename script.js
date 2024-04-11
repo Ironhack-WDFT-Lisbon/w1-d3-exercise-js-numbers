38985ad489ee34c463c3d1cf3236c7c49d2f2a1f
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-11 22:32:02 +0100

    update after class

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Ready to go! 🚀");
+
+// Exercise 1
+const resultSum = 7 + 5;
+const resultSubtraction = 7 - 5;
+
+// Exercise 2
+const resultsExponential = 2 ** 5;
+const resultModulus = 7 % 2;
+let resultAssignment = 7;
+resultAssignment += 3;
+
+// Exercise 3
+const grouping = (7 + 5) * 2;
+
+// Exercise 4
+const multipleVariables = resultSum + resultSubtraction;
+// const multipleVariables = ( 7 + 5 ) + ( 7 - 5 );
+// console.log(multipleVariables);
